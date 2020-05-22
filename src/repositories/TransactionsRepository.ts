aa3f794f5f84e713f8b4917d5d0f235cccb8400e
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-05-22 17:05:16 -0300

    Implementation

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (calcBalance, transaction) => {
+        const bal = calcBalance;
+        switch (transaction.type) {
+          case 'income':
+            bal.income += transaction.value;
+            break;
+
+          case 'outcome':
+            bal.outcome += transaction.value;
+            break;
+
+          default:
+            break;
+        }
+
+        return bal;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
